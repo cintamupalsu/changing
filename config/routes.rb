e30b8ffdc6a18556dbccea47da026ef7d6f5a06b
a_maulana <cintamupalsu@hotmail.com>
2018-11-25 15:25:26 +0900

    finish main pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'main_pages/home'
-
-  get 'main_pages/help'
+  root  'main_pages#home'
+  get   'main_pages/home'
+  get   'main_pages/help'
+  get    'main_pages/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'application#hello'
